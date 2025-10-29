fcf2d1be79c6badd036c0fe5429241f10e43aeb3
beardography <ryan.l.huber@gmail.com>
2025-10-28 20:33:24 -0500

    building out Project Card using Tailwind

---
+++ app/components/ProjectCard.vue
@@ -0,0 +1,73 @@
+<template>
+    <!-- Container -->
+    <div class="p-4 bg-pink-500 flex flex-col gap-4 w-full">
+        <!-- Title -->
+        <h2 class="font-eiko bg-amber-500 text-7xl">{{ title }}</h2>
+
+        <!-- Left -->
+        <div class="bg-green-500 flex fles-row gap-4">
+            <!-- Slider -->
+            <div class="bg-orange-500 flex flex-col gap-4 w-3/5">
+                <!-- Image -->
+                <img
+                    class="h-[677px] w-full object-cover"
+                    :src="image"
+                    :alt="title"
+                />
+
+                <!-- Slider Indicator -->
+                <div class="bg-yellow-500 flex flex-row gap-2">
+                    <div class="h-2 w-16 bg-blue-500 hover:bg-gray-800" />
+                    <div class="h-2 w-16 bg-blue-500 hover:bg-gray-800" />
+                    <div class="h-2 w-16 bg-blue-500 hover:bg-gray-800" />
+                    <div class="h-2 w-16 bg-blue-500 hover:bg-gray-800" />
+                </div>
+            </div>
+
+            <!-- Content -->
+            <div class="bg-purple-500 flex flex-col gap-4 w-2/5">
+                <!-- Description -->
+                <p class="font-neue-montreal text-2xl leading-[1.5]">
+                    {{ description }}
+                </p>
+
+                <!-- Link -->
+                <NuxtLink
+                    to="https://www.google.com"
+                    class="font-neue-montreal text-2xl font-bold text-black hover:text-gray-500 tracking-wide capitalize"
+                    >Read More <span class="ml-1">→</span></NuxtLink
+                >
+
+                <!-- Divider -->
+                <div class="h-[1px] width-full bg-gray-200 my-2" />
+
+                <!-- Tags -->
+                <div class="bg-neutral-500 flex flex-row gap-2">
+                    <Tag text="Tag 1" />
+                    <Tag text="Tag 2" />
+                    <Tag text="Tag 3" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineOptions({ name: "ProjectCard" });
+
+const { title, description } = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+        default: "",
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+});
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        <h1 class="font-eiko font-medium text-4xl">Home Page</h1>
-        <Link to="/about">About</Link>
-        <NuxtLink
+        <!-- <h1 class="font-eiko font-medium text-4xl">Home Page</h1> -->
+        <!-- <Link to="/about">About</Link> -->
+        <!-- <NuxtLink
             class="font-neue-montreal font-bold text-black hover:text-gray-500 text-2xl font-extrabold capitalize"
             to="/about"
             >About <span class="ml-2">→</span></NuxtLink
-        >
-        <Tag text="Hello" />
+        > -->
+        <!-- <Tag text="Hello" /> -->
+        <ProjectCard
+            title="Foundation - Templates"
+            image="https://images.unsplash.com/photo-1669295384050-a1d4357bd1d7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740"
+            description="Sit porta lacinia iaculis faucibus duis malesuada nisi. Mi aenean consectetur nulla iaculis; libero justo. Odio adipiscing lectus eget, lobortis neque integer. Proin vivamus accumsan accumsan; semper laoreet hac ipsum ridiculus mattis. Fringilla viverra gravida volutpat praesent tellus egestas cursus eros facilisis hac varius. Sollicitudin erat consectetur diam nibh ullamcorper imperdiet facilisis fringilla tellus libero; porta vivamus.
+
+Senectus tortor dolor neque vehicula tempor tristique faucibus eget mus phasellus. Augue phasellus justo ornare eget convallis egestas mi consequat massa. Tempus amet in placerat a hac dignissim; vivamus interdum condimentum lacus leo. Est volutpat cras urna vulputate eleifend viverra. Vestibulum vivamus adipiscing eu, morbi a viverra. Et mi hac aliquet bibendum fusce platea.
+
+"
+        />
     </div>
 </template>
